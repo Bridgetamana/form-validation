5d29e97bf2648f9c1f1aa31ccfb5d6d82c425333
Bridget Amana <amanabridget03@gmail.com>
2024-02-25 14:55:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const userName = document.getElementById('username')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm-password')
+const submitBtn = document.getElementById('submit-btn')
+
+// submitBtn.addEventListener('click', function (event) {
+//     if (password.value !== confirmPassword.value) {
+//         event.preventDefault()
+//         alert('passwords do not match')
+        
+//     }
+//     console.log('clicked')
+// })
