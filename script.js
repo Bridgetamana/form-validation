a798f9d76904411f0049e7ae469ff7da870a2130
Bridget Amana <amanabridget03@gmail.com>
2024-03-01 17:24:48 +0100

    Added email validation

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const userName = document.getElementById('username')
-const password = document.getElementById('password')
-const confirmPassword = document.getElementById('confirm-password')
-const submitBtn = document.getElementById('submit-btn')
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const username = document.getElementById('username');
+const submitBtn = document.getElementById('submit-btn');
+const emailError = document.getElementById('email-error');
 
-// submitBtn.addEventListener('click', function (event) {
-//     if (password.value !== confirmPassword.value) {
-//         event.preventDefault()
-//         alert('passwords do not match')
-        
-//     }
-//     console.log('clicked')
-// })
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+// Email Validation
+email.addEventListener("input", () => {
+  const isEmailValid = emailRegExp.test(email.value);
+  if (isEmailValid) {
+    email.className = "valid";
+    emailError.textContent = "";
+    emailError.style.display = "none";
+} else {
+    email.className = "invalid";
+    emailError.textContent = "Please enter a valid email address";
+    emailError.style.display = "block";
+}
+});
+
+// What happens when the user tries to submit the data
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const isEmailValid = email.value.length === 0 && emailRegExp.test(email.value);
+  if (isEmailValid) {
+    emailError.textContent = "";
+    emailError.style.display = "none";
+} else {
+    emailError.textContent = "I expect an email, darling!";
+    emailError.style.display = "block";
+}
+});
